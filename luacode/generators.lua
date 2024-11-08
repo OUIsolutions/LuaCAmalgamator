590f0f7e74ed44739c442eb95c05f0cf116a2c47
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-08 14:48:18 -0300

    full functions

--- luacode/generators.lua
+++ luacode/generators.lua
@@ -1,4 +1,4 @@
-private_lua_c_amalgamator.generate_amalgamation = function(filename, generator_callback, max_content_size, max_recursion)
+private_lua_c_amalgamator.generate_amalgamation = function(filename, max_content_size, max_recursion)
     if not filename then
         error("no filename provided")
     end
@@ -19,20 +19,41 @@ private_lua_c_amalgamator.generate_amalgamation = function(filename, generator_c
         error("max recursion its not a number")
     end
 
-    if not generator_callback then
-        return private_lua_c_amalgamator_cinterop.generate_amalgamation_simple(
-            filename,
-            max_content_size,
-            max_recursion
-        )
+    return private_lua_c_amalgamator_cinterop.generate_amalgamation_simple(
+        filename,
+        max_content_size,
+        max_recursion
+    )
+end
+
+private_lua_c_amalgamator.generate_amalgamation_verifier = function(filename, verifier, max_content_size, max_recursion)
+    if not filename then
+        error("no filename provided")
     end
-    if type(generator_callback) ~= "function" then
-        error("generator callback its not a function")
+    if type(verifier) ~= "function" then
+        error("verifier its not a function")
     end
+
+    if not max_content_size then
+        max_content_size = private_lua_c_amalgamator.ONE_MB * 10
+    end
+    if type(max_content_size) ~= "number" then
+        error("max content its not a number")
+    end
+
+
+    if not max_recursion then
+        max_recursion = 1000
+    end
+
+    if type(max_recursion) ~= "number" then
+        error("max recursion its not a number")
+    end
+
     return private_lua_c_amalgamator_cinterop.generate_amalgamation_complex(
         filename,
         max_content_size,
         max_recursion,
-        generator_callback
+        verifier
     )
 end
--- types/amalgamator_types.lua
+++ types/amalgamator_types.lua
@@ -5,7 +5,8 @@
 
 
 ---@class Amalgamator
----@field generate_amalgamation fun(filename:string,generator_callback:fun(include:string,path:string):"dont-include"| "dont-change" | "include-once" | "include-perpetual",max_content_size:number | nil,max_recursion:number|nil)
+---@field generate_amalgamation fun(filename:string, max_content_size:number | nil,max_recursion:number|nil)
+---@field generate_amalgamation_verifier fun(filename:string,verifier:fun(include:string,path:string):"dont-include"| "dont-change" | "include-once" | "include-perpetual",max_content_size:number | nil,max_recursion:number|nil)
 ---@field ONE_MB number
 ---@field ONE_BYTE number
 
